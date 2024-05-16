5805ee4d97070bff9312ee320442d6f46af5a24c
Dimas_Marcondes <dimasmarcondes88@gmail.com>
2024-05-16 14:38:47 -0300

    projeto_Doe_Simples_2024

---
+++ script.js
@@ -0,0 +1,19 @@
+function animarContador(id, start, end, duration) {
+    let obj = document.getElementById(id);
+    let current = start;
+    let range = end - start;
+    let increment = end > start ? 1 : -1;
+    let stepTime = Math.abs(Math.floor(duration / range));
+    let timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+window.onload = function() {
+    animarContador('contadorCrianças', 0, 180, 40000);
+    animarContador('contadorColaboradores', 0, 40, 40000);
+};
