88b90103120b51d14fd9b064490608b5502eb3c4
Dimas_Marcondes <dimasmarcondes88@gmail.com>
2024-08-08 12:45:18 -0300

    atualizando Doe_Simples

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ window.onload = function() {
     animarContador('contadorCrianças', 0, 180, 40000);
     animarContador('contadorColaboradores', 0, 40, 40000);
 };
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    const titulo = document.querySelector('.titulo');
+    const ongSection = document.querySelector('.ong_semente');
+    const ongSectionTop = ongSection.offsetTop;
+
+    if (window.scrollY >= ongSectionTop) {
+        header.classList.add('reduzido');
+        titulo.innerHTML = titulo.innerHTML.replace('Doe Simples', '<h3 class="titulo">Doe Simples</h3>');
+    } else {
+        header.classList.remove('reduzido');
+        titulo.innerHTML = titulo.innerHTML.replace('<h3 class="titulo">Doe Simples</h3>', 'Doe Simples');
+    }
+});
+
